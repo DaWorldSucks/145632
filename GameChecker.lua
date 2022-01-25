36f0f034f384f06f20c2d9c4b3c4097c91ddc02a
DaWorldSucks <54174699+DaWorldSucks@users.noreply.github.com>
2022-01-25 18:47:01 +0200

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -4,14 +4,10 @@ local BigPaintballId = 3527629287
 
 --Arsenal Hub Loader
 if GetGameId == ArsenalId then
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/DaWorldSucks/145632/main/67651352.lua"),true))()
-else 
-    print("Incorrect Game")
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/DaWorldSucks/145632/main/Arsenal.lua"),true))()
 end
 
 --Big Paintball Hub Loader
 if GetGameId == BigPaintballId then
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/DaWorldSucks/145632/main/67651352.lua"),true))()
-else
-    print("Incorrect Game")
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/DaWorldSucks/145632/main/Big%20Paintball"),true))()
 end
