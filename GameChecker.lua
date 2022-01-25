7b52abec45adbe040c2a283aae7b25c17a04354c
DaWorldSucks <54174699+DaWorldSucks@users.noreply.github.com>
2022-01-25 16:42:52 +0200

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -1,7 +1,6 @@
 local GetGameId = game.PlaceId
-local ArsenalId = 286090429
 
 --Arsenal Hub Loader
-if GetGameId == ArsenalId then
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/DaWorldSucks/145632/main/67651352.lua"),true))()
+if GetGameId == 286090429 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/DaWorldSucks/145632/main/Arsenal.lua"),true))()
 end
